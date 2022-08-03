861296d0f8dcc44dbb3c53358500480e34ae8e9f
Shantanu <shantanu1.mazumder@protonmail.com>
2022-08-03 10:04:14 -0400

    Show current day on the jumbotron

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
+// This function writes the current date in the jumbotron
+var displayCurrentDay = function(date){
+    $("#currentDay").text(date);
+};
 
+// This function get the current date and passes the formatted date to displayCurrentDay() to show on screen
+var showCurrentDay = function(){
+    var currentDay = moment();
+    displayCurrentDay(currentDay.format("dddd, MMMM Do YYYY"));
+};
+
+// The current day is shown on the screen as soon as the application starts
+showCurrentDay();
+// The current day will update every 5 seconds in case the user does not close the bowser for days at a time
+setInterval(showCurrentDay, 5000);
 
